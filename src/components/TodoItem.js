547b13d53f5f90a1304ec0b94d9547cf26448e0b
aabera <99598091+aabera@users.noreply.github.com>
2022-03-29 08:42:12 -0600

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class TodoItem extends React.Component {
+  render() {
+    return <li><input type="checkbox" checked={this.props.todo.completed} onChange={()=>console.log("clicked")}></input>{this.props.todo.title}</li>
+  }
+}
+
+export default TodoItem
+
+
